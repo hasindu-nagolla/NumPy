cd718307ebc4dd9644d34bdae047a1eda646e881
hasindu lakshan <hasindulakshan0040@gmail.com>
2025-06-23 18:12:41 +0530

    matrix creation

--- 03. Different Types of Arrays/Test.py
+++ 03. Different Types of Arrays/Test.py
@@ -4,20 +4,25 @@ import numpy as np
 zerosArray = np.zeros((10))
 print(zerosArray)
 
+
 # All 0s matrix with dimensions
 zerosArray2D = np.zeros((2, 2))
 print(zerosArray2D)
 
+
 zerosArray3D = np.zeros((2, 2, 2))
 print(zerosArray3D)  # we can create array with different shapes whatsoever we want
+
 
 # All 1s matrix
 onesArray = np.ones((10))
 print(onesArray)
 
+
 # All 1s matrix with dimensions
 onesArray2D = np.ones((2, 2))
 print(onesArray2D)
+
 
 # create matrixes with other numbers we want
 customArray = np.full((2, 2), 99)
@@ -26,9 +31,42 @@ print(customArray)
 
 # Create matrixes with other numbers we want using full_like
 x = np.array([[1, 2, 3], [4, 5, 6]])
-customArray = np.full_like(x, 99) # Creates a new array with the same shape and type as another array.
+# Creates a new array with the same shape and type as another array.
+customArray = np.full_like(x, 99)
 print(customArray)
 
+
 # Create a matrix using random decimal numbers
-randomDecimalArray = np.random.rand(4, 2) # creates a random numbers between 0 and 1
+# creates a random numbers between 0 and 1
+randomDecimalArray = np.random.rand(4, 2)
 print(randomDecimalArray)
+
+
+# Create a matrix using random integers
+# creates a random integers between 1 and 10
+randomIntegerArray = np.random.randint(1, 10, size=(3, 3))
+print(randomIntegerArray)
+
+
+# Create an Identity matrix
+# creates a 3x3 identity matrix, can be any size
+identityArray = np.identity(3)
+print(identityArray)
+
+
+# Repeat an array
+arrayRepeat = np.array([[1, 2, 3]])
+r1 = np.repeat(arrayRepeat, 3, axis=0)  # repeat each element 3 times
+print(r1)
+
+
+# Matrix embedding 
+output = np.ones((5, 5))
+print(output)
+
+output2 = np.zeros((3, 3))
+output2[1, 1] = 1
+print(output2)
+
+output[1:4, 1:4] = output2
+print(output)
